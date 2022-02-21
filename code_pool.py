dd2f9f28bffd8d584eee9de3e799c079e5a8a5d4
0xdomyz <yzdominic@outlook.com>
2022-02-21 21:17:11 +1100

    add codes

--- code_pool.py
+++ code_pool.py
@@ -1,7 +1,9 @@
 import warnings
+
 from contextlib import redirect_stdout
 import io
-
 with redirect_stdout(io.StringIO()) as f:
     help(pow)
 s = f.getvalue()
+
+
--- model/plot.py
+++ model/plot.py
@@ -1,3 +1,18 @@
 import seaborn as sns
 import matplotlib.pylab as plt
 
+plt.subplot()
+a, (b) = plt.subplots(gridspec_kw={})
+b.set_xlabel()
+b.set_xticks()
+b.set_xticklabels()
+b.twinx()
+plt.yscale()
+b.plot()
+b.legend()
+b.bar()
+plt.subplots_adjust()
+plt.close()
+
+
+
