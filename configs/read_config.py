0dd17a6fed4aeee5da3112dcff8a2052de539eca
unknown <yzdominic@outlook.com>
2022-04-12 19:11:22 +1000

    add config example

--- configs/read_config.py
+++ configs/read_config.py
@@ -1,29 +1,25 @@
+#python -i {this file}
 from configparser import ConfigParser
 from pathlib import Path
 
-pth = Path(__file__).parent / 'files/test'
-#pth = Path().parent / 'files/test'
-
-#read msg
-nme = 'oc'
-
-cfg = ConfigParser()
-url = None
-cfg.read(pth)
-if cfg.has_option('url',nme):
-    url = cfg.get('url',nme)
-print(url)
-
-#set msg
-msg = 'absdf'
-nme = 'oc'
+pth = Path(__file__).parent / 'config'
 
 cfg = ConfigParser()
 cfg.read(pth)
-if not cfg.has_section('url'):
-    cfg.add_section('url')
-cfg.set('url',nme,msg)
-with open(pth,'w') as f:
+
+for i in cfg:
+    print(i)
+
+cfg.get('section_9', 'part1')
+cfg.has_option('section_10', 'part1')
+cfg.has_option('section_10', 'part4')
+cfg.has_section('section_10')
+cfg.has_section('section_11')
+cfg.add_section('section_11')
+cfg.set('section_11','part2', '123')
+cfg['section_11']['part66']
+
+pth2 = pth.parent / 'config2'
+with open(pth2, 'w') as f:
     cfg.write(f)
-
-pth.unlink()
+pth2.unlink()
