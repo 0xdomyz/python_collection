226e81b924d1e773c68ac33345367a270115bcf5
unknown <yzdominic@outlook.com>
2022-04-13 01:03:03 +1000

    add env_reader

--- configs/read_env.py
+++ configs/read_env.py
@@ -1,5 +1,10 @@
 import os
 
-os.environ[''] = ''
-os.environ.get('')
+def env_reader(variable_name):
+    return os.environ.get(variable_name)
 
+if __name__ == "__main__":
+    os.environ['test'] = 'test'
+    os.environ.get('test')
+    p = env_reader("PATH")
+    p.split(";")
