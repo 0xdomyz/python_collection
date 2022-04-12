6b4b5e25dbb176233138907a5470275c6831311a
unknown <yzdominic@outlook.com>
2022-04-13 01:09:26 +1000

    fix paths

--- configs/paths.py
+++ configs/paths.py
@@ -1,90 +1,79 @@
-"""https://docs.python.org/3/library/pathlib.html"""
-from pathlib import Path
-
-f = __file__
-
-p = Path(f).parent
-
-Path(__file__).parents[2]
-
-ap = p.resolve().as_posix()
-ap
-
-for i in p.iterdir():
-    print(i)
-
-for i in p.iterdir():
-    if i.isdir():
-        print(i)
-
-(p / 'files').resolve()
-
-(p / 'files').exists()
-
-(p / 'file').exists()
-
-with open(p / 'files/data.yml') as f:
-    print(f)
-
-for i in p.glob('*.py'):
-    print(i)
-
-for i in p.glob('*/*.py'):
-    print(i)
-
-for i in p.glob('**/*.yml'):
-    print(i)
-
-p.home()
-
-(p.home() / '.dwopt').resolve()
-
-p.home().joinpath('.dwopt').resolve()
-
-[i for i in p.glob('files/*.yml')]
-
-[i for i in p.glob('files/*ta.yml')]
-
-q = p.joinpath('paths.py')
-aq = q.resolve().as_posix()
-aq
-
-q.stem
-
-q.suffix
-
-q.name
-
-q.with_name('read_env.py')
-
-
-""""""
 import os
-
-os.path.abspath('')
-
-os.path.abspath('../files/data.yml')
-
-os.path.dirname(aq)
-
-with open(os.path.join(ap,'files','data.yml')) as f: print(f)
-
-
-"""https://docs.python.org/3/library/glob.html"""
+from pathlib import Path
 from glob import glob
-
-glob(ap+'/**/*.yml')
-
-
-""""""
 import sys
 
-sys.path
 
-sys.path.insert(0,p)
-
-
-""""""
 if __name__ == '__main__':
-    path = Path(__file__).parent / 'files/data.yml'
+    f = __file__
+
+    p = Path(f).parent
+
+    Path(__file__).parents[2]
+
+    ap = p.resolve().as_posix()
+    ap
+
+    for i in p.iterdir():
+        print(i)
+
+    for i in p.iterdir():
+        if i.is_dir():
+            print(i)
+
+    (p / 'files').resolve()
+
+    (p / 'files').exists()
+
+    (p / 'file').exists()
+
+    with open(p / 'config.yaml') as f:
+        print(f)
+
+    for i in p.glob('*.py'):
+        print(i)
+
+    for i in p.glob('*/*.py'):
+        print(i)
+
+    for i in p.glob('**/*.yaml'):
+        print(i)
+
+    p.home()
+
+    (p.home() / '.dwopt').resolve()
+
+    p.home().joinpath('.dwopt').resolve()
+
+    [i for i in p.glob('*.yaml')]
+
+    [i for i in p.glob('*ta.yaml')]
+
+    q = p.joinpath('paths.py')
+    aq = q.resolve().as_posix()
+    aq
+
+    q.stem
+
+    q.suffix
+
+    q.name
+
+    q.with_name('read_env.py')
+
+    os.path.abspath('')
+
+    os.path.abspath('../configs/config.yaml')
+
+    os.path.dirname(aq)
+
+    with open(os.path.join(ap,'config.yaml')) as f: print(f)
+
+    glob(ap+'/**/*.yaml')
+    
+    sys.path
+
+    sys.path.insert(0,p)
+
+    path = Path(__file__).parent / 'config.yaml'
     print(path)
