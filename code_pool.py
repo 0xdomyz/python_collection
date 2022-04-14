692e5d55d3da5c06c2eb1b71951595900a802cb0
unknown <yzdominic@outlook.com>
2022-04-14 21:23:18 +1000

    move code to shell

--- code_pool.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-from contextlib import redirect_stdout
-import io
-with redirect_stdout(io.StringIO()) as f:
-    help(pow)
-s = f.getvalue()
-
-
---
+++ shell/contextlib.py
@@ -0,0 +1,8 @@
+from contextlib import redirect_stdout
+import io
+
+if __name__ == "__main__":
+   with redirect_stdout(io.StringIO()) as f:
+       help(pow)
+   s = f.getvalue()
+   print(s)
