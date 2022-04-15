e971d8c34249ef4ca5b6674b9f81f8f65c08fbcf
unknown <yzdominic@outlook.com>
2022-04-16 01:37:40 +1000

    add decorator examples

---
+++ higher_lvl_func/fancy_decorator.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+import math
+
+
+@dataclass
+class PlayingCard:
+    rank: str
+    suit: str
+
+if __name__ == "__main__":
+    card = PlayingCard('ace', 'black')
