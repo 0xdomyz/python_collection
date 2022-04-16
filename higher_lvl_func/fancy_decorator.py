1e5e4dd9f2ba964efd98b4d07003d05286593a9f
unknown <yzdominic@outlook.com>
2022-04-16 19:18:16 +1000

    add decorator exmaple

--- higher_lvl_func/fancy_decorator.py
+++ higher_lvl_func/fancy_decorator.py
@@ -11,11 +11,21 @@ def repeat(num_times):
         return new_func
     return decorate
 
+def count_calls(func):
+    @functools.wraps(func)
+    def wrapper_count_calls(*args, **kwargs):
+        wrapper_count_calls.num_calls += 1
+        print(f"Call {wrapper_count_calls.num_calls} of {func.__name__!r}")
+        return func(*args, **kwargs)
+    wrapper_count_calls.num_calls = 0
+    return wrapper_count_calls
+
 
 @dataclass
 class PlayingCard:
     rank: str
     suit: str
+
 
 @repeat(num_times=4)
 def greet(name):
@@ -25,7 +35,15 @@ def greet(name):
 def greet2(name):
     print(f"Hello {name}")
 
+@count_calls
+def say_whee():
+    print("Whee!")
+
 if __name__ == "__main__":
     card = PlayingCard("ace", "black")
     greet("aaa")
     greet2("bbb")
+    say_whee()
+    say_whee()
+    say_whee()
+    print(say_whee.num_calls)
