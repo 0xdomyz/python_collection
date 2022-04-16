0c720479dcf923c1245cfcdca1b43a29ab5582b5
unknown <yzdominic@outlook.com>
2022-04-16 19:01:59 +1000

    add fancy docorate exmaple

--- higher_lvl_func/fancy_decorator.py
+++ higher_lvl_func/fancy_decorator.py
@@ -1,5 +1,15 @@
 from dataclasses import dataclass
-import math
+import functools
+
+def repeat(num_times):
+    def decorate(func):
+        @functools.wraps(func)
+        def new_func(*args, **kwargs):
+            for _ in range(num_times):
+                value = func(*args, **kwargs)
+            return value
+        return new_func
+    return decorate
 
 
 @dataclass
@@ -7,6 +17,15 @@ class PlayingCard:
     rank: str
     suit: str
 
+@repeat(num_times=4)
+def greet(name):
+    print(f"Hello {name}")
+
+@repeat(5)
+def greet2(name):
+    print(f"Hello {name}")
 
 if __name__ == "__main__":
     card = PlayingCard("ace", "black")
+    greet("aaa")
+    greet2("bbb")
