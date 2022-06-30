c552b93973ad084cb9c7d3a6388d9b98cc0fb74f
unknown <yzdominic@outlook.com>
2022-07-01 00:30:39 +1000

    fix db tute

--- data/db_tute.py
+++ data/db_tute.py
@@ -4,7 +4,7 @@ https://www.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip
 
 from dwopt import db
 
-d = db("sqlite:///chinook.db")
+d = db("sqlite:///data/chinook.db")
 
 
 d.list_tables()
