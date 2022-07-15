8c6e9cb827de95eaa5b9e5768cdfe6005ce2022c
unknown <yzdominic@outlook.com>
2022-07-15 23:47:06 +1000

    add type hint tute

---
+++ layout/type_hint_tute.py
@@ -0,0 +1,27 @@
+
+a: int = 3
+b: float = 3.14
+c: str = 'abc'
+d: bool = False
+e: list = ['a', 'b', 'c']
+f: tuple = (1, 2, 3)
+g: dict = {'a': 1, 'b': 2}
+
+from typing import List, Tuple, Dict
+e: List[str] = ['a', 'b', 'c']
+f: Tuple[int, int, int] = (1, 2, 3)
+g: Dict[str, int] = {'a': 1, 'b': 2}
+
+def func(a: str, b: float, c: dict, d: tuple, e: list) -> dict:
+    res = {
+        "a":a,
+        "b":b,
+        "c":c,
+        "d":d,
+        "e":e
+    }
+    return res
+
+func("aaa",2.0,{"a":1},(1,2),[1,3])
+print(func.__annotations__)
+
