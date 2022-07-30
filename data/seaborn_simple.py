7e2a68ca2048539dec388b6915e276aad1260987
unknown <yzdominic@outlook.com>
2022-07-31 02:18:30 +1000

    update seaborn

--- data/seaborn_simple.py
+++ data/seaborn_simple.py
@@ -11,57 +11,50 @@ list(titanic.select_dtypes("number").columns)
 #['survived', 'pclass', 'age', 'sibsp', 'parch', 'fare']
 list(titanic.select_dtypes(object).columns)
 #['sex', 'embarked', 'class', 'who', 'deck', 'embark_town', 'alive']
-
+head(titanic)
 
 
 #statistical relationship
 
 #    scatter
+sns.relplot(data=titanic, x="age", y="fare")
+sns.relplot(data=titanic, x="age", y="fare",hue="survived")
+sns.relplot(data=titanic, x="age", y="fare",col="survived")
+sns.relplot(data=titanic, x="age", y="fare",style="survived")
+sns.relplot(data=titanic, x="age", y="fare",size="survived")
+
+sns.relplot(data=titanic, x="age", y="fare",hue="survived",style="survived",col="class")
 
 #    line: continuity
-
+sns.relplot(data=titanic, x="age", y="fare",kind="line")
+sns.relplot(data=titanic, x="age", y="fare",kind="line",hue="survived",style="survived",col="class")
 
 #variable distribution
 
 #    hist
+sns.displot(data=titanic,x="age")#continuouse
+sns.displot(data=titanic,x="class")#discrete
+sns.displot(data=titanic,x="age",y="fare")
+sns.displot(data=titanic, x="alive", y="class")
 
+sns.displot(data=titanic,x="age",hue="class")
+sns.displot(data=titanic,x="age",col="survived")
+sns.displot(data=titanic,x="age",hue="survived",col="class")
+
+sns.displot(data=titanic,x="class",hue="survived")
+sns.displot(data=titanic,x="class",col="embark_town")
+sns.displot(data=titanic,x="class",hue="survived",col="embark_town")
 
 #categorical data
 
 #    box
+sns.catplot(data=titanic, x="alive", y="age", kind="box")
 
 #    point
+sns.catplot(data=titanic, x="alive", y="age")
 
 #    bar
-
-
-#additional variables to any of the base charts
-#hue
-#style
-#size
-#col
-
-
-#univariate
-
-#    continuouse/numerical
-sns.displot(data=titanic,x="pclass")
-sns.displot(data=titanic,x="age")
-sns.displot(data=titanic,x="fare")
-
-#    discrete/categorical
 sns.catplot(data=titanic, x="alive", col="deck", kind="count")
 
 
-#bi-variate
-
-#    continuous vs continuous
-sns.relplot(data=titanic, x="age", y="fare")
-sns.displot(data=titanic, x="age", y="fare")
-
-#    discrete vs continuous
-sns.catplot(data=titanic, x="alive", y="age")
-
-#    distcret vs discrete
-sns.displot(data=titanic, x="alive", y="class")
 
