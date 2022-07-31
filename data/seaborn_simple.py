837d1d7ef5867a0dd1200479f16c699620429293
unknown <yzdominic@outlook.com>
2022-08-01 00:51:37 +1000

    add seaborn

--- data/seaborn_simple.py
+++ data/seaborn_simple.py
@@ -7,11 +7,14 @@ import seaborn as sns
 
 titanic = pd.read_csv("toydata/titanic.csv")
 
-list(titanic.select_dtypes("number").columns)
-#['survived', 'pclass', 'age', 'sibsp', 'parch', 'fare']
-list(titanic.select_dtypes(object).columns)
-#['sex', 'embarked', 'class', 'who', 'deck', 'embark_town', 'alive']
 titanic.head()
+
+list(titanic.select_dtypes("number").columns)
+list(titanic.select_dtypes(object).columns)
+list(titanic.select_dtypes(bool).columns)
+
+#numeric: ['age', 'fare']
+#categorical: ['survived', 'sibsp', 'parch', 'class', 'who', 'deck', 'embark_town', 'adult_male', 'alone']
 
 
 #statistical relationship
@@ -29,32 +32,49 @@ sns.relplot(data=titanic, x="age", y="fare",hue="survived",style="survived",col=
 sns.relplot(data=titanic, x="age", y="fare",kind="line")
 sns.relplot(data=titanic, x="age", y="fare",kind="line",hue="survived",style="survived",col="class")
 
+
 #variable distribution
 
 #    hist
-sns.displot(data=titanic,x="age")#continuouse
-sns.displot(data=titanic,x="class")#discrete
-sns.displot(data=titanic,x="age",y="fare")
-sns.displot(data=titanic, x="alive", y="class")
+
+#        continuouse
+sns.displot(data=titanic,x="age")
+sns.displot(data=titanic,x="age", y="fare")
 
 sns.displot(data=titanic,x="age",hue="class")
-sns.displot(data=titanic,x="age",col="survived")
-sns.displot(data=titanic,x="age",hue="survived",col="class")
+sns.displot(data=titanic,x="age",col="alive")
+sns.displot(data=titanic,x="age",hue="alive",col="class")
 
-sns.displot(data=titanic,x="class",hue="survived")
-sns.displot(data=titanic,x="class",col="embark_town")
-sns.displot(data=titanic,x="class",hue="survived",col="embark_town")
+sns.displot(data=titanic,x="age", y="fare", col="alive")
+
+#        discrete
+sns.displot(data=titanic,x="alive")
+sns.displot(data=titanic,x="class",y="alive")
+
+sns.displot(data=titanic,x="alive", col="class")
+sns.displot(data=titanic,x="alive", col="who")
+sns.displot(data=titanic,x="alive", col="adult_male")
+sns.displot(data=titanic,x="alive", col="alone")
+sns.displot(data=titanic,x="alive", col="embark_town")
+sns.displot(data=titanic,x="alive", col="parch", col_wrap=3)
+sns.displot(data=titanic,x="alive", col="sibsp", col_wrap=3)
+
+sns.displot(data=titanic,x="class",y="alive",col="who")
+
 
 #categorical data
 
 #    box
 sns.catplot(data=titanic, x="alive", y="age", kind="box")
+sns.catplot(data=titanic, x="deck", y="age", kind="box")
 
 #    point
 sns.catplot(data=titanic, x="alive", y="age")
+sns.catplot(data=titanic, x="deck", y="age")
 
 #    bar
-sns.catplot(data=titanic, x="alive", col="deck", kind="count")
+sns.catplot(data=titanic, x="alive", kind="count")
+sns.catplot(data=titanic, x="alive", col="class", kind="count")
 
 
 
