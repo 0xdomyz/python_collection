ce0c764d974554c73272abcf3ba3a2be898b71f7
unknown <yzdominic@outlook.com>
2022-08-18 21:45:18 +1000

    update layout script example

--- layout/scripts/a_script.py
+++ layout/scripts/a_script.py
@@ -6,6 +6,7 @@ Examples
     python layout\scripts\a_script.py abc
     python layout\scripts\a_script.py abc 123
     python layout\scripts\a_script.py -s abc
+    python layout\scripts\a_script.py -l 1 2 3 abc
 """
 from argparse import ArgumentParser
 from pathlib import Path
@@ -16,9 +17,10 @@ logging.basicConfig(level=20)
 parser = ArgumentParser()
 parser.add_argument("input1", help="input1 does nothing")
 parser.add_argument("-s", "--special", action="store_true")
+parser.add_argument("-l", action="extend", nargs="+", type=str)
 
 def main(input1):
-    print(input1)
+    print(f"{input1 = }")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -28,3 +30,4 @@ if __name__ == "__main__":
     else:
         main(args.input1)
 
+    print(args.l)
