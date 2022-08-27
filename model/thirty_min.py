888854ec2d1a22820a26c9078792d7387b790397
0xdomyz <yzdominic@outlook.com>
2022-08-27 23:08:42 +1000

    add theiry min model code

--- model/thirty_min.py
+++ model/thirty_min.py
@@ -6,11 +6,10 @@ import category_encoders as ce
 import sklearn.preprocessing as prep
 #from sklearn.preprocessing import StandardScaler
 import statsmodels.api as sm
-import sklearn.linear_model as sklm
 import sklearn.metrics as metrics
 
 
-#data & first look
+#data
 import seaborn as sns
 df = sns.load_dataset("titanic")
 df.head()
@@ -34,15 +33,15 @@ cat_vars =[
     "alone",
 ]
 
-junk = [
+duplicated_columns = [
      "alive",
      "pclass",
      "embarked",
 ]
 
 
-#pre-pre-processing
-X:pd.DataFrame = df.drop(junk+[target], axis=1).loc[:,num_vars + cat_vars]
+#create X and y modelling frames
+X:pd.DataFrame = df.drop(duplicated_columns+[target], axis=1).loc[:,num_vars+cat_vars]
 y:pd.Series = df[target]
 
 X.head()
@@ -56,26 +55,46 @@ encoder = ce.WOEEncoder(cols=cols)
 encoder.fit(X[cols], y)
 X[cols] = encoder.transform(X[cols])
 
-X
+X.head()
+X.dtypes
 
-#   dummy
-pd.get_dummies(X["who"])
 
-drop_enc = prep.OneHotEncoder(drop='first').fit(X[cols])
-drop_enc.transform(X[cols]).toarray()
+#discretize numeric variables
+est = prep.KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='quantile')
+est.fit(X[["fare"]])
+X["fare_bin"] = est.transform(X[["fare"]])
 
-#   other encoders
-def encode(encoder_class, X, cols, y):
-    encoder = encoder_class(cols=cols)
-    encoder.fit(X[cols], y)
-    return encoder.transform(X[cols])
+_ = (
+    prep.KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='uniform')
+    .fit(X[["fare"]])
+    .transform(X[["fare"]])
+)
+pd.DataFrame(_).value_counts()
 
-cols = ["who"]
-encode(ce.TargetEncoder, X, cols, y)
-df.groupby(cols).agg(avg_survive = ("survived",np.mean))
+X["fare_bin"].value_counts()
 
-encode(ce.LeaveOneOutEncoder, X, cols, y)
-encode(ce.GLMMEncoder, X, cols, y)#doco says this is log odds if target is binary
+
+#dummy and other encoders
+if False:
+
+    #   dummy
+    pd.get_dummies(X["who"])
+
+    drop_enc = prep.OneHotEncoder(drop='first').fit(X[cols])
+    drop_enc.transform(X[cols]).toarray()
+
+    #   other encoders
+    def encode(encoder_class, X, cols, y):
+        encoder = encoder_class(cols=cols)
+        encoder.fit(X[cols], y)
+        return encoder.transform(X[cols])
+
+    cols = ["who"]
+    encode(ce.TargetEncoder, X, cols, y)
+    df.groupby(cols).agg(avg_survive = ("survived",np.mean))
+
+    encode(ce.LeaveOneOutEncoder, X, cols, y)
+    encode(ce.GLMMEncoder, X, cols, y)#doco says this is log odds if target is binary
 
 
 #standardise
@@ -86,7 +105,8 @@ X = scaler.transform(X)
 X_df = pd.DataFrame(X, columns=cols)
 
 X
-X.shape
+X_df.head()
+
 scaler.mean_
 np.sqrt(scaler.var_)
 
@@ -97,32 +117,48 @@ X_df = X_df.dropna()
 X = X_df.values
 y = y.loc[X_df.index]
 
+X_df.head()
+X_df.shape
+
+
 #statsmodels
 estimator = sm.GLM(endog = y, exog = X_df, family = sm.families.Binomial())
-res = estimator.fit()
+fitted_glm_model = estimator.fit()
 
-res.summary()
-res.get_prediction().summary_frame(alpha=0.05)
+fitted_glm_model.summary()
+fitted_glm_model.params.abs()/sum(fitted_glm_model.params.abs())*100
 
+fitted_glm_model.get_prediction().summary_frame(alpha=0.05)
+predictions = fitted_glm_model.get_prediction().summary_frame()["mean"]
+
+
+#evaluation
+
+#    accuracy
+import scipy
+scipy.stats.somersd(y, predictions).statistic
+
+
+#    calibration
+predictions_calibrated = np.where(predictions>0.5, 1, 0)
+
+_ = pd.concat([
+    df.loc[X_df.index].assign(
+        survived=predictions_calibrated,
+        prediction_actual="prediction"
+    ),
+    df.loc[X_df.index].assign(
+        survived=y,
+        prediction_actual="actual"
+    ),
+])
+
+import matplotlib.pyplot as plt
+sns.catplot(data=_, x="survived", hue = "prediction_actual",kind="count")
+sns.catplot(data=_, x="survived", hue = "prediction_actual",col="adult_male",kind="count")
+sns.catplot(data=_, x="survived", hue = "prediction_actual",col="class",kind="count")
+plt.show()
 
 #sklearn
-sklm.LogisticRegression, sklm.LogisticRegressionCV
-metrics.roc_curve, metrics.auc, metrics.r2_score
 
-lr = LogisticRegression()
-
-lr.fit(X,y)
-
-lr.coef_
-lr.intercept_
-
-wts = np.round(np.abs(lr.intercept_)/sum(lr.intercept_)*100,2)
-
-p = lr.predict()
-
-r2_score(y, p)
-
-n=len(p)
-k = len(lr.coef_)
-adj_r2 = 1 - (1-r2) * float(n-1) / float(n-k-1)
 
