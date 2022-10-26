c5fdd9e014ff213f97b6322e342fd20764a60eec
0xdomyz <yzdominic@outlook.com>
2022-10-26 21:31:07 +1100

    duckdb add sample up

--- data/duckdb.py
+++ data/duckdb.py
@@ -1,6 +1,8 @@
 import datetime
+import random
 
 import pandas
+import pandas as pd
 
 import duckdb
 
@@ -46,14 +48,7 @@ on a.dte between b.start and b.end
 
 con.execute(sql).df()
 
-
-import datetime
-import random
-
-import pandas as pd
-
-import duckdb
-
+# more data
 dt = datetime.datetime(year=2021, month=10, day=1)
 length = 3000
 random.seed(0)
@@ -90,3 +85,53 @@ on a.time between b.start_time and b.end_time
 )
 
 len(res)
+
+
+# sample up
+times = pd.Series(
+    [
+        datetime.datetime.fromisoformat("2022-01-01 12:06:47"),
+        datetime.datetime.fromisoformat("2022-02-03 12:06:47"),
+        datetime.datetime.fromisoformat("2022-03-17 12:06:47"),
+    ]
+)
+
+units = pd.Series([1, 3, 7])
+
+df = pd.DataFrame(
+    {
+        "time": times,
+        "unit": units,
+    }
+)
+df = df.reset_index(drop=True)
+df["time_lead"] = df["time"].shift(-1)
+df.loc[len(df) - 1, "time_lead"] = pd.Timestamp("2022-12-31")
+df
+
+grid = pd.DataFrame(
+    {
+        "time": pd.date_range(
+            "2022-01-01 00:00:00", "2022-12-31 00:00:00", freq="D"
+        ).astype("datetime64[ns]")
+    }
+)
+grid["time"]
+
+res = (
+    duckdb.connect()
+    .execute(
+        """
+    select
+        a.*,
+        b.unit
+    from grid a
+    left join df b
+    on a.time > b.time and a.time <= b.time_lead
+    order by a.time
+"""
+    )
+    .df()
+)
+
+res.to_clipboard()
