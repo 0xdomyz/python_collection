4abbc7c5727a7143dda9b73a83fdff72b388ae26
0xdomyz <yzdominic@outlook.com>
2022-11-17 22:54:09 +1100

    add stremlit simple

--- dashboard/streamlit_tute/simple.py
+++ dashboard/streamlit_tute/simple.py
@@ -1,9 +1,10 @@
+import numpy as np
 import pandas as pd
 import streamlit as st
 
 
 def get_df():
-    df = pd.DataFrame({"x": [1, 2, 3, 4], "y": [10, 20, 30, 40]})
+    df = pd.DataFrame({"x": [1, 2, 3, 4], "y": np.random.randn(4)})
     return df
 
 
@@ -13,5 +14,10 @@ Here's our first attempt at using data to create a table:
 """
 
 df = get_df()
+
+df
+st.write(df)
+st.table(df)
+
 st.line_chart(df)
 number = st.slider("Pick a number", 0, 100)
