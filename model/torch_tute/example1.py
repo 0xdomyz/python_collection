8362555fcd366ed9b85c1e0e11963c491c7a05ee
0xdomyz <yzdominic@outlook.com>
2022-11-22 00:32:21 +1100

    udpate torch tute

---
+++ model/torch_tute/example1.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.utils
+
+config = dict()
+x_train = 1
+y_train = 1
+
+torch.manual_seed(0)
+torch.sigmoid()
+torch.relu()
+torch.set_num_threads(8)
+torch.tensor(x_train)
+torch.float
+torch.optim.adam()
+torch.ones()
+
+nn.ModuleList()
+nn.Dropout()
+nn.BCEWithLogitsLoss()
+nn.Linear()
+nn.Module
+nn.BCELoss()
