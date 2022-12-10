e6fd8d9d5921af92cc2555a5cabf3ed07307068f
0xdomyz <yzdominic@outlook.com>
2022-12-11 00:01:41 +1100

    update matplot tute

--- plot/matplot.py
+++ plot/matplot.py
@@ -4,7 +4,7 @@ import numpy as np
 
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
-
+plt.show()
 
 fig = plt.figure()  # an empty figure with no Axes
 fig, ax = plt.subplots()  # a figure with a single Axes
