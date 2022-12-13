76a2080a09d1785847ef648329b384a29c932f01
0xdomyz <yzdominic@outlook.com>
2022-12-13 12:28:09 +1100

    update pandas tute

--- data/pandas_tute/pandas_others.py
+++ data/pandas_tute/pandas_others.py
@@ -1,5 +1,5 @@
-import numpy as npo
-import pandas as np
+import numpy as np
+import pandas as pd
 
 # swaplevel
 
@@ -27,3 +27,41 @@ df = pd.DataFrame(
 print(df)
 print(df.ffill())
 print(df.bfill())
+
+# example of combining multiple columns and count unique values in the combined field
+df = pd.DataFrame(
+    {
+        "A": [1, 2, 3, 4, 5, 6],
+        "B": [1, 2, 3, np.nan, np.nan, np.nan],
+        "C": [1, 2, 3, 4, 5, 6],
+    }
+)
+
+df["combined"] = df["A"].astype(str) + df["B"].astype(str) + df["C"].astype(str)
+df["combined"].value_counts()
+
+# example of join 2 tables on multiple columns
+df1 = pd.DataFrame(
+    {
+        "A": [1, 2, 3, 4, 5, 6],
+        "B": [1, 2, 3, np.nan, np.nan, np.nan],
+        "C": [1, 2, 3, 4, 5, 6],
+    }
+)
+df2 = pd.DataFrame(
+    {
+        "A": [1, 2, 3, 4, 5, 6],
+        "B": [1, 2, 3, np.nan, np.nan, np.nan],
+        "C": [1, 2, 3, 4, 5, 6],
+    }
+)
+
+df1.merge(df2, on=["A", "B", "C"])
+
+# merge above on A, B and bring C values from df2 into result
+df1.merge(df2, on=["A", "B"], suffixes=("", "_y"))
+
+# combine 2 columns with 1 taking precedence if another is null
+df1["combined"] = np.where(df1["A"].isnull(), df1["B"], df1["A"])
+
+# example of conditional merge that joins df2 if value of A is between value A of df1
