69c5f34e877330b13c425589d31876e1558254bc
0xdomyz <yzdominic@outlook.com>
2022-12-20 23:07:17 +1100

    udpate streamlit simple

--- dashboard/streamlit_tute/simple.py
+++ dashboard/streamlit_tute/simple.py
@@ -3,21 +3,26 @@ import pandas as pd
 import streamlit as st
 
 
-def get_df():
-    df = pd.DataFrame({"x": [1, 2, 3, 4], "y": np.random.randn(4)})
+def get_df(number):
+    df = pd.DataFrame({"x": [1, 2, 3, 4], "y": number * np.random.randn(4)})
     return df
 
 
 """
 # My first app
 Here's our first attempt at using data to create a table:
+
 """
 
-df = get_df()
+url = "https://en.wikipedia.org/wiki/Main_Page"
+f"[{url}]({url})"
+
+
+number = st.slider("Pick a number", 0, 100, 50)
+df = get_df(number)
 
 df
 st.write(df)
 st.table(df)
 
 st.line_chart(df)
-number = st.slider("Pick a number", 0, 100)
