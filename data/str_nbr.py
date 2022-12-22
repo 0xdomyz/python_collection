67ac678fa3f4531c0f7a8f87465732863014651c
0xdomyz <yzdominic@outlook.com>
2022-12-22 16:13:38 +1100

    udpate str nbr

--- data/str_nbr.py
+++ data/str_nbr.py
@@ -14,3 +14,22 @@ print(int2str(1, 2))
 print(int2str(9, 2))
 print(int2str(10, 2))
 print(int2str(15, 2))
+
+
+# convert fixed length string to int
+def str2int(s):
+    return int(s)
+
+
+# use pd to dummify arbituary str
+import pandas as pd
+
+df = pd.DataFrame({"A": ["a", "b", "c", "a"]})
+print(pd.get_dummies(df))
+
+
+# factorise str into int representation
+df = pd.DataFrame({"A": ["a", "b", "c", "a"]})
+print(df["A"].factorize())
+df.loc[:, "A_fac"] = df["A"].factorize()[0]
+df
