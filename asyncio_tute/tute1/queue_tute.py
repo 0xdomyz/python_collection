e04f109cfe01a983779f5da9fbb9a077ed12f123
0xdomyz <yzdominic@outlook.com>
2023-01-08 20:49:41 +1100

    update asyncio queue tute

--- asyncio_tute/tute1/queue_tute.py
+++ asyncio_tute/tute1/queue_tute.py
@@ -10,14 +10,34 @@ async def makeitem(size: int = 5) -> str:
 
 
 async def randsleep(caller=None) -> None:
-    i = random.randint(0, 10)
+    """Sleep for a random amount of time between 0 and 2 seconds.
+
+    Example::
+
+        asyncio.run(randsleep(caller="Producer 1"))
+    """
+    i = random.randint(0, 2)
     if caller:
         print(f"{caller} sleeping for {i} seconds.")
     await asyncio.sleep(i)
 
 
 async def produce(name: int, q: asyncio.Queue) -> None:
+    """Produce random data and put it into the queue.
+
+    Example::
+
+        async def main():
+            q = asyncio.Queue()
+            # have 2 producers
+            await asyncio.gather(produce(1, q), produce(2, q))
+            #print out q current size
+            print(f"q size: {q.qsize()}")
+
+        asyncio.run(main())
+    """
     n = random.randint(0, 10)
+    print(f"Producer {name} will produce {n} items.")
     for _ in it.repeat(None, n):  # Synchronous loop for each single producer
         await randsleep(caller=f"Producer {name}")
         i = await makeitem()
@@ -27,11 +47,34 @@ async def produce(name: int, q: asyncio.Queue) -> None:
 
 
 async def consume(name: int, q: asyncio.Queue) -> None:
+    """Produce random data and put it into the queue.
+
+    Example::
+
+        async def main():
+            q = asyncio.Queue()
+            # put 2 items into the queue
+            await q.put((1, time.perf_counter()))
+            await q.put((2, time.perf_counter()))
+            # 2 consumer as task
+            consumers = [asyncio.create_task(consume(1, q)),
+                            asyncio.create_task(consume(2, q))]
+            # wait for the queue to be empty
+            await q.join()
+            # cancel the consumers
+            for c in consumers:
+                c.cancel()
+            #print out q current size
+            print(f"q size: {q.qsize()}")
+
+        asyncio.run(main())
+    """
     while True:
         await randsleep(caller=f"Consumer {name}")
         i, t = await q.get()
         now = time.perf_counter()
         print(f"Consumer {name} got element <{i}>" f" in {now-t:0.5f} seconds.")
+        print(f"q size: {q.qsize()}")
         q.task_done()
 
 
