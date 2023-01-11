be472e154be722a06de5adc7e1de9258633935a7
0xdomyz <yzdominic@outlook.com>
2023-01-12 00:00:27 +1100

    reorg and add loguru logging scripts

---
+++ log/logging_to_loguru.py
@@ -0,0 +1,34 @@
+# intercept logging msg to loguru
+##################################
+import logging
+import sys
+
+from loguru import logger
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists.
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = sys._getframe(6), 6
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
+
+logging_logger = logging.getLogger(__name__)
+logging_logger.setLevel(logging.INFO)
+
+logging_logger.info("This is a test message")
+logging_logger.debug("This is a test message")
--- log/loguru_to_logging.py
+++ log/loguru_to_logging.py
@@ -2,13 +2,8 @@ import logging
 
 from loguru import logger
 
-# sys handler
-# import logging.handlers
-# handler = logging.handlers.SysLogHandler(address=("localhost", 514))
-# logger.remove()
-# logger.add(handler, level="INFO")
-
-# logging
+# logging usage
+#############################################
 logging_logger = logging.getLogger(__name__)
 logging_logger.setLevel(logging.INFO)
 logging.basicConfig()
@@ -17,20 +12,14 @@ logging_logger.info("This is a test message")
 logging_logger.debug("This is a test message")
 
 # propagate loguru to logging
-logger.remove()
-logger.add(logging_logger.info, level="INFO")
-
-# alternatively
-logger.remove()
-
-
+####################################
 class PropagateHandler(logging.Handler):
     def emit(self, record):
         logging.getLogger(record.name).handle(record)
 
 
+logger.remove()
 logger.add(PropagateHandler(), format="{message}", level="INFO")
 
-# use loguru
 logger.info("This is a test message")
 logger.debug("This is a test message")
