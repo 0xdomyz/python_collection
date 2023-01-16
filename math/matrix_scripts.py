013761afba23acd4a889b17a0105d4a471b0f070
0xdomyz <yzdominic@outlook.com>
2023-01-16 21:10:50 +1100

    udpate matrix

--- math/matrix_scripts.py
+++ math/matrix_scripts.py
@@ -5,14 +5,25 @@ A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 A
 
 # sum over rows
+####################
 A.sum(axis=1)
+
+# condition
+######################
 
 # test if sum over row is not 0
 A.sum(axis=1) != 0
 # as 1, else 0
 np.where(A.sum(axis=1) != 0, 1, 0)
 
+# if cell < 3, return 0, else 1
+np.where(A < 3, 0, 1)
+
+# if cell < 5, return 0, else if cell < 7, return 1, else 2
+np.where(A < 5, 0, np.where(A < 7, 1, 2))
+
 # element wise sum of 2 arrays
+###################################
 A = np.array([1, 2, 3])
 B = np.array([4, 5, 6])
 
@@ -20,6 +31,7 @@ lsts = [A, B]
 np.sum(lsts, axis=0)
 
 # matrix add 1 more row
+###################################
 A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 A
 
@@ -30,10 +42,13 @@ np.vstack((A, zeros))
 np.vstack((A, np.array([10, 11, 12])))
 
 # fill na with 0
+###################################
 B = np.array([[1, 2, 3], [4, 5, 6], [7, 8, np.nan]])
 B
 np.nan_to_num(B)
 
+# reshape
+###################################
 
 # ratio over sum of rows
 A / A.sum(axis=1).reshape(-1, 1)
@@ -45,30 +60,32 @@ A - A.mean(axis=1).reshape(-1, 1)
 (A - A.mean(axis=1).reshape(-1, 1)) / A.std(axis=1).reshape(-1, 1)
 
 # cumulative sum over rows
+################################
 A.cumsum(axis=1)
-
-# reversed
-A[:, ::-1]
 
 # cumulative sum over rows reversed
 A[:, ::-1].cumsum(axis=1)[:, ::-1]
 
-# if cell < 3, return 0, else 1
-np.where(A < 3, 0, 1)
+# reversed
+###################
+A[:, ::-1]
 
-# if cell < 5, return 0, else if cell < 7, return 1, else 2
-np.where(A < 5, 0, np.where(A < 7, 1, 2))
 
 # normalize whole matrix
+#############################
 A_normalized = (A - A.mean()) / A.std()
 
 # standard normal inverse
+#############################
 from scipy.stats import norm
 
 norm.ppf(0.975)
 norm.ppf(0.11)
 A_row_pct = A / A.sum(axis=1).reshape(-1, 1)
 norm.ppf(A_row_pct)
+
+# diagonals
+####################
 
 # upper diagonal
 np.triu(A, k=1)
@@ -102,6 +119,9 @@ np.triu(A, k=0) + np.tril(np.ones(A.shape), k=-1) * 100
 # test a condition only on upper triangular matrix incl diagonal
 np.triu(A, k=0) > 5
 
+# conditions and diagonals
+#########################
+
 # test if matrix is monotonic increasing over columns
 A = np.array(np.random.randint(0, 10, size=(5, 5)))
 A
@@ -132,6 +152,7 @@ np.all(np.tril(np.diff(A), k=0) <= 0)
 
 
 # test if matrix is symmetric
+#############################
 A = np.array(np.random.randint(0, 10, size=(5, 5)))
 A
 A.T
@@ -186,3 +207,32 @@ for i, j in zip([0] + split_points, split_points):
         print(i, j, k, l)
         print(labels[i:j], labels[k:l])
         print(A[i:j, k:l])
+
+# matrix algebra
+####################
+A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+B = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# matrix multiplication
+A @ B
+
+# matrix multiplication
+np.dot(A, B)
+
+# matrix inverse
+np.linalg.inv(A)
+
+# matrix determinant
+np.linalg.det(A)
+
+# matrix trace
+np.trace(A)
+
+# matrix transpose
+A.T
+
+# matrix rank
+np.linalg.matrix_rank(A)
+
+# matrix eigenvalues
+np.linalg.eigvals(A)
