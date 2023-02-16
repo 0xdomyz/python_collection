75f3e3a925d08e680563af3c10adb0ccb8faf8f4
0xdomyz <yzdominic@outlook.com>
2023-02-16 21:25:47 +1100

    udpate wallpaper

--- web/wallpaper/wallpaper.py
+++ web/wallpaper/wallpaper.py
@@ -52,6 +52,38 @@ def open_folder():
         print("Error opening folder")
 
 
+def save_run_date(date: datetime.date):
+    with open(path / "run_date.txt", "w") as f:
+        f.write(date.strftime("%Y-%m-%d"))
+
+
+def get_run_date():
+    file = path / "run_date.txt"
+    if not file.exists():
+        return None
+    else:
+        with open(file, "r") as f:
+            date = f.read()
+        return datetime.datetime.strptime(date, "%Y-%m-%d").date()
+
+
+def save_date_actions(date: datetime.date):
+    prior_date = get_run_date()
+
+    if prior_date is None:
+        get_wallpaper(date)
+        save_run_date(date)
+    else:
+        # get wallpapers for all weeks between saved date and current date
+        dates = [
+            date - datetime.timedelta(weeks=i)
+            for i in range((date - prior_date).days // 7 + 1)
+        ]
+        for date in dates:
+            get_wallpaper(date)
+        save_run_date(date)
+
+
 if __name__ == "__main__":
     # use argparse to get day, month, year
     parser = argparse.ArgumentParser()
@@ -62,14 +94,32 @@ if __name__ == "__main__":
     parser.add_argument(
         "--weeks_prior", type=int, default=None, help="Number of weeks to extend range"
     )
-    args = parser.parse_args()
+    # optional argument to save date to file and use saved date to get weeks prior
+    parser.add_argument(
+        "--save_date",
+        action="store_true",
+        help="Save date to file to auto use weeks prior, weeks prior must not be set",
+    )
 
+    # make sure weeks prior is not set if save date is set
+    args = parser.parse_args()
+    if args.save_date and args.weeks_prior is not None:
+        parser.error("--save_date and --weeks_prior cannot be set at the same time")
+
+    # if date is not set, use today's date
     if args.date is None:
         date = datetime.date.today()
     else:
         year, month, day = args.date.split("-")
         date = datetime.date(int(year), int(month), int(day))
 
+    # vanilla run, get wallpaper for date
+    if args.weeks_prior is None and not args.save_date:
+        get_wallpaper(date)
+        open_folder()
+        exit()
+
+    # if weeks prior is set, get wallpapers for that many weeks prior
     if args.weeks_prior is not None:
         # list of dates
         dates = [
@@ -77,7 +127,11 @@ if __name__ == "__main__":
         ]
         for date in dates:
             get_wallpaper(date)
-    else:
-        get_wallpaper(date)
+        open_folder()
+        exit()
 
-    open_folder()
+    # if save date is set, do save date actions
+    if args.save_date:
+        save_date_actions(date)
+        open_folder()
+        exit()
