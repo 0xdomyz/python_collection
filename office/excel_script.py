86992b616b1cf104d93c67980735469ea0533344
0xdomyz <yzdominic@outlook.com>
2023-05-16 08:20:56 +1000

    udpate excel script

--- office/excel_script.py
+++ office/excel_script.py
@@ -69,3 +69,36 @@ ws.conditional_formatting.add(
 )
 
 wb.save(f"{here}/table3.xlsx")
+
+
+# functionalise
+def save_to_excel_with_color_scale(data: pd.DataFrame, filename):
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    # fill in the data
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            cell = ws.cell(row=i + 2, column=j + 2)
+            cell.value = data.iloc[i, j]
+
+    # add the color scale
+    ws.conditional_formatting.add(
+        f"B2:E{data.shape[0]+1}",
+        openpyxl.formatting.rule.ColorScaleRule(
+            start_type="min",
+            start_color="00FF0000",
+            end_type="max",
+            end_color="0000FF00",
+        ),
+    )
+
+    wb.save(filename)
+
+
+data = pd.DataFrame(
+    np.random.randn(10, 4),
+    columns=list("ABCD"),
+)
+
+save_to_excel_with_color_scale(data=data, filename=f"{here}/table4.xlsx")
