eeba87bf7ed078fa5fafe8ab287f33bd19da9f07
0xdomyz <yzdominic@outlook.com>
2023-06-25 12:49:54 +1000

    update move file into folder by reverswesw

--- shell/move_file_into_folders.py
+++ shell/move_file_into_folders.py
@@ -26,3 +26,17 @@ def move_contains_into_a_folder(contains:str, folder:str,here:Path):
         print(f.name)
         f.rename(here / folder / f.name)
 
+# reverse by moving files out of folders
+def move_files_out_of_folders(here:Path):
+    for i in [i for i in here.iterdir() if i.is_dir()]:
+        # i is a folder in here
+        for j in [j for j in i.iterdir()]:
+            # j is a file or folder in i
+            if j.is_dir():
+                raise Exception("There is a folder in a folder")
+            else:
+                print(j)
+                j.rename(here / j.name)
+        i.rmdir()
+
+
