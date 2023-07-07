b61e573e4e2a5fe51e0ac9b11969ed218c124f76
0xdomyz <yzdominic@outlook.com>
2023-07-07 22:39:19 +1000

    upate json tute

--- configs/json_tute.py
+++ configs/json_tute.py
@@ -1,3 +1,76 @@
+# from tutorial
+#####################
+import json
+
+# list, dict, str, int, float, True, False, None, tuple
+json.dumps(["foo", {"bar": ("baz", None, 1.0, 2)}])
+
+json.dumps([1,2,3])
+json.dumps((1,2,3))# no difference between tuple and list
+
+print(json.dumps('"foo\bar'))
+
+print(json.dumps("\u1234"))
+
+print(json.dumps("\\"))
+
+# sort
+print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True))
+
+# stream
+from io import StringIO
+
+io = StringIO()
+json.dump(["streaming API"], io)
+io.getvalue()
+
+# compact
+import json
+
+json.dumps([1, 2, 3, {"4": 5, "6": 7}])
+json.dumps([1, 2, 3, {"4": 5, "6": 7}], separators=(",", ":"))# get rid of space
+
+# pretty
+import json
+
+print(json.dumps({"4": 5, "6": 7}, sort_keys=True))
+json.dumps({"4": 5, "6": 7}, sort_keys=True, indent=4)
+print(json.dumps({"4": 5, "6": 7}, sort_keys=True, indent=4))# spaces and \n
+
+# decode
+import json
+
+json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
+
+json.loads('"\\"foo\\bar"')
+
+from io import StringIO
+
+io = StringIO('["streaming API"]')
+json.load(io)
+
+
+# specialize
+import json
+
+
+def as_complex(dct):
+    if "__complex__" in dct:
+        return complex(dct["real"], dct["imag"])
+    return dct
+
+
+json.loads('{"__complex__": true, "real": 1, "imag": 2}')
+json.loads('{"__complex__": true, "real": 1, "imag": 2}', object_hook=as_complex)
+
+import decimal
+
+json.loads("1.1", parse_float=decimal.Decimal)
+
+
+# some custom funcs
+############################
+
 # example of save and load json file of list of dict
 import json
 from pathlib import Path
