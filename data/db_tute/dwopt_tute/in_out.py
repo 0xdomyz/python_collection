b09d2f10d613d528c1c3d030878d93b56eff152a
0xdomyz <yzdominic@outlook.com>
2023-09-22 20:27:25 +1000

    finalise in_out

--- data/db_tute/dwopt_tute/in_out.py
+++ data/db_tute/dwopt_tute/in_out.py
@@ -28,3 +28,33 @@ df2 = df.loc[random.sample(range(100), 60), :].copy()
 primary_keys = ["id", "date"]
 
 combined = df1.merge(df2, on=primary_keys, how="outer", indicator=True)
+combined
+
+# do via sql
+db.cwrite(df1, "df1")
+db.cwrite(df2, "df2")
+
+left_table_name = "df1"
+right_table_name = "df2"
+
+primary_keys = ["id", "date"]
+
+join_cls = " and\n    ".join(f"l.{pk} = r.{pk}" for pk in primary_keys)
+right_pk_select_cls = ",\n    ".join(f"r.{pk} as right_{pk}" for pk in primary_keys)
+sql = f"""
+select
+    l.*,
+    {right_pk_select_cls},
+    case when r.{primary_keys[0]} is null then 'left_only'
+         when l.{primary_keys[0]} is null then 'right_only'
+         else 'both' end as _merge
+from {left_table_name} l
+full outer join {right_table_name} r
+on {join_cls}
+"""
+
+print(sql)
+
+df = db.run(sql)
+
+df.value_counts("_merge")
