19c77713f7508a9759bcbdf8af272617362dc0d6
0xdomyz <yzdominic@outlook.com>
2025-08-02 18:04:46 +0100

    excel writer allows run as context man or not

--- data/analysis/excel_writer.py
+++ data/analysis/excel_writer.py
@@ -4,38 +4,38 @@ import pandas as pd
 from loguru import logger
 from contextlib import contextmanager
 
-VERSION = "2025.08.02.16"
+VERSION = "2025.08.02.18"
 
 
 class ExcelWriter(object):
     def __init__(
         self,
         file_path: Path,
+        mode: str = "w",
         sheet_name: str = "Sheet",
         cur_row: int = 1,
         cur_col: int = 1,
         distance_bw_tables: int = 2,
-        mode: str = None,
     ):
         self.file_path = file_path
+        self.writer = None
         self.mode = mode
+
+        # custom attributes
         self.sheet_name = sheet_name
-        self.start_row = 1
-        self.start_col = 1
         self.cur_row = cur_row
         self.cur_col = cur_col
         self.distance_bw_tables = distance_bw_tables
 
-        if not self.file_path.exists():
-            if self.mode and self.mode == "a":
-                raise ValueError(
-                    f"File {self.file_path} does not exist, cannot open in append mode."
-                )
-            self.mode = "w"
+        # hardcoded attributes
+        self.start_row = 1
+        self.start_col = 1
+        self._write_df_called = False
 
     def __repr__(self):
         return (
-            f"ExcelWriter(file_path='{self.file_path}', sheet_name={self.sheet_name}),"
+            f"ExcelWriter(file_path='{self.file_path}', mode = {self.mode},"
+            f" sheet_name={self.sheet_name}),"
             f" cur_row={self.cur_row}, cur_col={self.cur_col}, distance_bw_tables={self.distance_bw_tables})"
         )
 
@@ -53,31 +53,87 @@ class ExcelWriter(object):
     #     else:
     #         logger.warning("ExcelWriter was not opened, nothing to close.")
 
-    def __enter__(self):
-        logger.debug(f"Opening ExcelWriter in {self.mode} mode for {self.file_path}")
+    def __enter__(self, mode: str = None, reset_cursor: bool = True):
+        """
+        ways to enter:
+        1. obj as context manager
+        2. manually first entry
+        3. manually subsequent entries
+        """
+        mode = mode or self.mode
+
+        if not self.file_path.exists():
+            mode = "w"
+            logger.debug(f"File {self.file_path} does not exist, force mode w.")
+
+        logger.debug(f"Enter ExcelWriter: {mode = }")
         self.writer = pd.ExcelWriter(
             self.file_path,
             engine="openpyxl",
-            mode=self.mode,
-            if_sheet_exists="overlay" if self.mode == "a" else None,
+            mode=mode,
+            if_sheet_exists="overlay" if mode == "a" else None,
         )
-        self.switch_to_existing_or_new_sheet(self.sheet_name)
+        self.writer._status = "open"  # inplant
+        self.switch_to_sheet(
+            self.sheet_name, create_if_not_exists=True, reset_cursor=reset_cursor
+        )
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        logger.debug("Exiting ExcelWriter context manager")
+        logger.debug("Exit ExcelWriter")
         self.writer.close()
+        self.writer._status = "closed"
 
-    def switch_to_existing_or_new_sheet(self, sheet_name: str):
+    def _make_new_sheet(self, sheet_name: str):
+        logger.debug(f"Creating new sheet: {sheet_name}")
+        assert (
+            sheet_name not in self.writer.book.sheetnames
+        ), f"Sheet {sheet_name} already exists in the workbook."
+        self.ws = self.writer.book.create_sheet(sheet_name)
+        return self
+
+    def switch_to_sheet(
+        self,
+        sheet_name: str,
+        create_if_not_exists: bool = False,
+        reset_cursor: bool = True,
+    ):
+        if create_if_not_exists and sheet_name not in self.writer.book.sheetnames:
+            logger.debug(f"Sheet {sheet_name} does not exist, creating it.")
+            self._make_new_sheet(sheet_name)
+
         logger.debug(f"Switching to sheet: {sheet_name}")
-        if sheet_name not in self.writer.book.sheetnames:
-            self.writer.book.create_sheet(sheet_name)
+        assert (
+            sheet_name in self.writer.book.sheetnames
+        ), f"Sheet {sheet_name} does not exist in the workbook."
         self.ws = self.writer.sheets[sheet_name]
         self.sheet_name = sheet_name
+        if reset_cursor:
+            self._reset_cursor()
+        return self
+
+    def _reset_cursor(self):
+        logger.debug(f"Resetting cursor to {self.start_row}, {self.start_col}")
         self.cur_row = self.start_row
+        self.cur_col = self.start_col
         return self
 
     def write_df(self, df: pd.DataFrame, title: str = None, index=True):
+
+        if self.writer is None or self.writer._status != "open":
+            logger.debug("ExcelWriter is not open, opening it now for this method.")
+            self.__enter__(
+                mode="a" if self._write_df_called else None,
+                reset_cursor=False if self._write_df_called else True,
+            )
+            try:
+                self.write_df(df, title, index)
+            except Exception as e:
+                raise e
+            finally:
+                self.__exit__(None, None, None)
+            return self
+
         logger.debug(
             f"Writing {title} on {self.sheet_name} at R{self.cur_row}, C{self.cur_col}"
         )
@@ -96,4 +152,6 @@ class ExcelWriter(object):
         )
 
         self.cur_row += df.shape[0] + self.distance_bw_tables
+
+        self._write_df_called = True
         return self
