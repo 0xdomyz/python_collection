02eb804f2e83996d45cac1fb15654782d6a68092
0xdomyz <yzdominic@outlook.com>
2025-08-17 22:43:53 +0100

    tm improve complexity

--- plot/plotting/pylibs/transition_matrix.py
+++ plot/plotting/pylibs/transition_matrix.py
@@ -7,71 +7,82 @@ VERSION = "2025.08.17.21"
 
 
 def plot_transition_matrix(
-    transition_matrix_df: pd.DataFrame,
-    color_transition_matrix_df: pd.DataFrame = None,
+    tm_df: pd.DataFrame,
+    color_tm_df: pd.DataFrame = None,
+    bottom_tm_df: pd.DataFrame = None,
+    tm_formatter: callable = lambda x: f"{x:.0f}" if x != 0 else "",
+    bottom_tm_formatter: callable = lambda x: f"{x:.2%}" if x != 0 else "",
     title="",
-    display_transition_rates=True,
     ax=None,
     figsize=(6, 5),
     fontsize=5,
+    fontsize_bottom=None,
     colorbar_label=None,
+    colorbar_formatter: callable = lambda x, _: f"{x*100:.0f}%",
 ):
 
-    def text_formatter(x):
-        return f"{x:.0f}" if x != 0 else ""
-
-    def pct_formatter(x):
-        return f"{x:.2%}" if x != 0 else ""
-
-    pdf = transition_matrix_df.copy()
-    pdf_color = (
-        color_transition_matrix_df.copy()
-        if color_transition_matrix_df is not None
-        else pdf.div(pdf.sum(axis=1), axis=0)
-    )
+    # inputs
+    if color_tm_df is None:
+        color_tm_df = tm_df.div(tm_df.sum(axis=1), axis=0).fillna(0)
     assert (
-        pdf.shape == pdf_color.shape
-    ), f"Transition matrix and color transition matrix must have the same shape, they are: {pdf.shape} vs {pdf_color.shape}"
+        tm_df.shape == color_tm_df.shape
+    ), f"TM and color TM must have the same shape, they are: {tm_df.shape} vs {color_tm_df.shape}"
 
+    if bottom_tm_df is not None:
+        assert (
+            tm_df.shape == bottom_tm_df.shape
+        ), f"TM and bottom TM must have the same shape, they are: {tm_df.shape} vs {bottom_tm_df.shape}"
+
+    fontsize_bottom = fontsize if fontsize_bottom is None else fontsize_bottom
+
+    # ax
     if ax is None:
         fig, ax = plt.subplots(figsize=figsize)
     else:
         fig = ax.figure
 
-    yticks, yticklabels, ylabel = range(len(pdf.index)), pdf.index, pdf.index.name
-    xticks, xticklabels, xlabel = range(len(pdf.columns)), pdf.columns, pdf.columns.name
+    # ticks
+    yticks, yticklabels, ylabel = range(len(tm_df.index)), tm_df.index, tm_df.index.name
+    xticks, xticklabels, xlabel = (
+        range(len(tm_df.columns)),
+        tm_df.columns,
+        tm_df.columns.name,
+    )
 
-    cax = ax.imshow(pdf_color.values, cmap="Blues")
-    color_threshold = pdf_color.values.max() / 2
+    # color
+    cax = ax.imshow(color_tm_df.values, cmap="Blues")
+    color_threshold = color_tm_df.values.max() / 2
 
+    # texts
     for i in yticks:
         for j in xticks:
-            color_value = pdf_color.values[i, j]  # row i, col j from top left
+            color_value = color_tm_df.values[i, j]  # row i, col j from top left
             text_color = "white" if color_value > color_threshold else "black"
 
-            va = "bottom" if display_transition_rates else "center"
             # plot in x pos j, y pos i from top left
             ax.text(
                 j,
                 i,
-                text_formatter(pdf.iloc[i, j]),
+                tm_formatter(tm_df.iloc[i, j]),
                 ha="center",
-                va=va,
+                va="center" if bottom_tm_df is None else "bottom",
                 color=text_color,
                 fontsize=fontsize,
             )
 
-            if display_transition_rates:
+            # bottom
+            if bottom_tm_df is not None:
                 ax.text(
                     j,
                     i,
-                    pct_formatter(color_value),
+                    bottom_tm_formatter(bottom_tm_df.iloc[i, j]),
                     ha="center",
                     va="top",
                     color=text_color,
-                    fontsize=fontsize,
+                    fontsize=fontsize_bottom,
                 )
 
+    # misc plot elements
     ax.plot(
         [0, max(xticks)], [max(yticks), 0], color="black", linewidth=0.5, linestyle="--"
     )  # bot left is origin in labels
@@ -86,18 +97,15 @@ def plot_transition_matrix(
 
     ax.set_title(title)
 
-    # Add minor ticks at halfway points
     ax.set_xticks(np.arange(-0.5, len(xticks), 1), minor=True)
     ax.set_yticks(np.arange(-0.5, len(yticks), 1), minor=True)
-
-    # Show grid at minor ticks (halfway points between cells)
     ax.grid(which="minor", color="gray", linestyle="--", linewidth=0.5)
 
     if colorbar_label:
         fig.colorbar(
             cax,
             label=colorbar_label,
-            format=mticker.FuncFormatter(lambda x, _: f"{x*100:.0f}%"),
+            format=mticker.FuncFormatter(colorbar_formatter),
             orientation="vertical",
             pad=0.02,
         )
