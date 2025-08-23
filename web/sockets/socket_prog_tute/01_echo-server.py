243bfea01b462f731b936d8b0481ac10ee7284f4
0xdomyz <yzdominic@outlook.com>
2025-08-23 23:29:44 +0100

    start to go thru socket examples

--- web/sockets/socket_prog_tute/01_echo-server.py
+++ web/sockets/socket_prog_tute/01_echo-server.py
@@ -7,12 +7,13 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
+    s.listen()  # make listening socket
+    conn, addr = s.accept()  # block and wait
+    with conn:  # use the new socket
         print(f"Connected by {addr}")
         while True:
-            data = conn.recv(1024)
-            if not data:
+            data = conn.recv(1024)  # blocking
+            if not data:  # signal of client disconnect
                 break
             conn.sendall(data)
+    # go out of with, socket is closed
--- web/sockets/socket_prog_tute/02_echo-client.py
+++ web/sockets/socket_prog_tute/02_echo-client.py
@@ -9,5 +9,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b"Hello, world")
     data = s.recv(1024)
+    # go out of with, socket is closed
 
 print(f"Received {data!r}")
