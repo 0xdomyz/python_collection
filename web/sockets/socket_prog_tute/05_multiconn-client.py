f507be01a1596d209d344642a8c410d73f698b9b
0xdomyz <yzdominic@outlook.com>
2025-08-24 18:46:27 +0100

    update multi conn eg

--- web/sockets/socket_prog_tute/05_multiconn-client.py
+++ web/sockets/socket_prog_tute/05_multiconn-client.py
@@ -4,6 +4,7 @@ import selectors
 import socket
 import sys
 import types
+from loguru import logger
 
 sel = selectors.DefaultSelector()
 messages = [b"Message 1 from client.", b"Message 2 from client."]
@@ -13,7 +14,7 @@ def start_connections(host, port, num_conns):
     server_addr = (host, port)
     for i in range(0, num_conns):
         connid = i + 1
-        print(f"Starting connection {connid} to {server_addr}")
+        logger.info(f"Starting connection {connid} to {server_addr}")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setblocking(False)
         sock.connect_ex(server_addr)
@@ -22,7 +23,7 @@ def start_connections(host, port, num_conns):
             connid=connid,
             msg_total=sum(len(m) for m in messages),
             recv_total=0,
-            messages=messages.copy(),
+            messages=messages.copy(),  # each conn will modify list
             outb=b"",
         )
         sel.register(sock, events, data=data)
@@ -34,23 +35,23 @@ def service_connection(key, mask):
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
-            print(f"Received {recv_data!r} from connection {data.connid}")
+            logger.info(f"Received {recv_data!r} from connection {data.connid}")
             data.recv_total += len(recv_data)
         if not recv_data or data.recv_total == data.msg_total:
-            print(f"Closing connection {data.connid}")
+            logger.info(f"Closing connection {data.connid}")
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
         if not data.outb and data.messages:
             data.outb = data.messages.pop(0)
         if data.outb:
-            print(f"Sending {data.outb!r} to connection {data.connid}")
+            logger.info(f"Sending {data.outb!r} to connection {data.connid}")
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
 
 
 if len(sys.argv) != 4:
-    print(f"Usage: {sys.argv[0]} <host> <port> <num_connections>")
+    logger.info(f"Usage: {sys.argv[0]} <host> <port> <num_connections>")
     sys.exit(1)
 
 host, port, num_conns = sys.argv[1:4]
@@ -66,6 +67,6 @@ try:
         if not sel.get_map():
             break
 except KeyboardInterrupt:
-    print("Caught keyboard interrupt, exiting")
+    logger.info("Caught keyboard interrupt, exiting")
 finally:
     sel.close()
