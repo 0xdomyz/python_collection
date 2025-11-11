a1a720a0224653e386853c82557a7f3d7e79c08f
0xdomyz <yzdominic@outlook.com>
2025-11-11 00:08:00 +0000

    add aio docu details

---
+++ concurrency_related/asyncio_script/docu/coroutines_tasks_related/awaitables_future.py
@@ -0,0 +1,46 @@
+import asyncio
+import concurrent.futures
+from pathlib import Path
+
+
+def blocking_io():
+    # File operations (such as logging) can block the
+    # event loop: run them in a thread pool.
+    with open(Path().cwd() / "awaitables.ipynb", "rb") as f:
+        return f.read(100)
+
+
+def cpu_bound():
+    # CPU-bound operations will block the event loop:
+    # in general it is preferable to run them in a
+    # process pool.
+    return sum(i * i for i in range(10**7))
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    ## Options:
+
+    # 1. Run in the default loop's executor:
+    result = await loop.run_in_executor(None, blocking_io)
+    print("default thread pool", result)
+
+    # 2. Run in a custom thread pool:
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool, blocking_io)
+        print("custom thread pool", result)
+
+    # 3. Run in a custom process pool:
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool, cpu_bound)
+        print("custom process pool", result)
+
+    # 4. Run in a custom interpreter pool:
+    # with concurrent.futures.InterpreterPoolExecutor() as pool:
+    #     result = await loop.run_in_executor(pool, cpu_bound)
+    #     print("custom interpreter pool", result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
