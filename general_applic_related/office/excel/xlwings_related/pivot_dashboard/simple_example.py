cb3ca69a5f3ec9aa6295d1bcb8ca845905ecd709
0xdomyz <yzdominic@outlook.com>
2026-04-12 04:50:40 +0100

    Refactor data processing in simple_example.py to use make_binned_column and improve DataFrame sorting

--- general_applic_related/office/excel/xlwings_related/pivot_dashboard/simple_example.py
+++ general_applic_related/office/excel/xlwings_related/pivot_dashboard/simple_example.py
@@ -3,40 +3,23 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import xlwings as xw
+from piv_dash_utils import make_binned_column
 from xlwings_pivot_dashboard import PivotDashboard
 
 # %%
 # initial data
 df = sns.load_dataset("titanic")
-df_info = pd.concat(
-    [df.head(1).T, df.dtypes.astype(str), df.nunique(), df.isna().sum()], axis=1
-)
+
+df_info = pd.concat([df.head(1).T, df.dtypes.astype(str), df.nunique(), df.isna().sum()], axis=1) # fmt: skip
 df_info.columns = ["example_value", "dtypes", "nunique", "n_null"]
-df_info = df_info.sort_values(
-    [
-        "nunique",
-        "dtypes",
-    ]
-)
+df_info = df_info.sort_index()
 print(df_info.to_string())
 # %%
-df["age_group"] = pd.cut(df["age"], bins=[0, 18, 40, 80]).astype(str)
-# %%
-edges = np.histogram_bin_edges(df["fare"].fillna(0), bins="auto")
-max_digits = max(len(str(int(edges.max()))), len(str(int(edges.min()))))
-
-
-def interval_to_padded_str(interval):
-    if not hasattr(interval, "left"):
-        return "nan"
-    left = str(int(interval.left)).zfill(max_digits)
-    right = str(int(interval.right)).zfill(max_digits)
-    return f"({left}, {right}]"
-
-
-df["fare_binned"] = (
-    pd.cut(df["fare"].fillna(0), bins=edges).map(interval_to_padded_str).astype(str)
+df["age_group"] = make_binned_column(df["age"], bins=20, make_padded_str=True)
+df["fare_binned"] = make_binned_column(
+    df["fare"], bins="auto", fill_value=0, make_padded_str=True
 )
+df["n"] = 1
 
 # %% [markdown]
 # ### simple usage
@@ -47,21 +30,13 @@ dashboard = PivotDashboard(wb)
 dashboard.write_table(df)
 
 PIVOT_CONFIGS = [
-    dict(row_field="who", col_field="survived", data_field="fare", xl_func="count"),
-    dict(
-        row_field="age_group",
-        col_field="survived",
-        data_field="fare",
-        xl_func="count",
-        chart_type="area_stacked",
-    ),
-    dict(
-        row_field="fare_binned",
-        col_field="survived",
-        data_field="fare",
-        xl_func="count",
-        chart_type="area_stacked",
-    ),
+    # fmt: off
+    dict(row_field="who", col_field="survived", data_field="n"),
+    dict(row_field="embark_town", col_field="survived", data_field="n"),
+    dict(row_field="age_group",col_field="survived",data_field="n",chart_type="area_stacked",), 
+    dict(row_field="fare_binned",col_field="survived",data_field="n",chart_type="area_stacked",),
+    dict(row_field="deck", col_field="survived", data_field="n"),
+    # fmt: on
 ]
 dashboard.add_pivots(PIVOT_CONFIGS)
 
