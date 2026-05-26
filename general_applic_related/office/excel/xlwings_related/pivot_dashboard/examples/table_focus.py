83e5c4b51e1d23d842df6c55274d618cf5f78c4b
0xdomyz <yzdominic@outlook.com>
2026-05-26 23:41:58 +0100

    Add table_focus.py for pivot dashboard example using xlwings

---
+++ general_applic_related/office/excel/xlwings_related/pivot_dashboard/examples/table_focus.py
@@ -0,0 +1,66 @@
+# %%
+import pathlib
+import sys
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import xlwings as xw
+
+sys.path.append(str(pathlib.Path().cwd().parent))
+from xlwings_pivot_dashboard import PivotDashboard
+
+# %%
+# initial data
+df = sns.load_dataset("titanic")
+
+df["age_group"] = pd.cut(
+    df["age"], bins=[0, 10, 20, 30, 40, 50, 60, 70, 80], labels=False
+).astype("str")
+df["fare_binned"] = pd.qcut(df["fare"], q=10, labels=False)
+df["n"] = 1
+
+print(df.shape)
+print(df.head(3).T.to_string())
+
+# %% [markdown]
+# ### xxx
+
+
+# %%
+wb = xw.Book()
+dashboard = PivotDashboard(wb)
+dashboard.write_table(df, code=f"SELECT * FROM df")
+
+pivot_configs = [
+    # fmt: off
+    dict(data_field=["n",'survived'],xl_func=['sum','average'],row_field="who",plot_on_2nd_axis='survived'),
+
+    dict(data_field=["n",'survived'], xl_func=['sum','average'],row_field="age_group",col_field="who",chart_type="area_stacked",plot_on_2nd_axis=['survived']),
+    # fmt: on
+]
+chart_layout = {
+    "ncols": 1,
+}
+pivot_dest = {
+    "col": "N",
+    "start_row": 5,
+    "row_step": 30,
+    "ncols": 1,
+}
+dashboard.add_pivots(pivot_configs, chart_layout, pivot_dest)
+
+slicer_layout = {
+    "left_offset": 1400,
+}
+
+dashboard.add_slicers(
+    fields=[
+        "class",
+        "embark_town",
+    ],
+    layout=slicer_layout,
+)
+
+# %%
+# wb.close()
