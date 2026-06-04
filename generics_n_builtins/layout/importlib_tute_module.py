c31b6db959b23ece2c4dd0555df89a4f7ffb879c
0xdomyz <yzdominic@outlook.com>
2026-06-04 09:43:37 +0100

    Add example_module with a simple function and demonstrate module reloading

--- generics_n_builtins/layout/importlib_tute_module.py
+++ generics_n_builtins/layout/importlib_tute_module.py
@@ -1,2 +1,24 @@
-def func():
-    print("func() in importlib_tute_module.py")
+# %%
+
+import importlib
+
+mod = importlib.import_module("example_module")
+
+# %%
+mod.func(3)
+
+# %% [markdown]
+# ## reload only
+# ####################################################################################################
+# %%
+
+import example_module as em
+
+em.func(3)
+
+# %%
+import importlib
+
+importlib.reload(em)
+
+em.func(3)
