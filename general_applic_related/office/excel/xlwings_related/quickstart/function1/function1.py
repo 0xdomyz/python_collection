8e1fd65a14e0a851bcd2e7df22e07f84be6f0043
0xdomyz <yzdominic@outlook.com>
2026-06-21 18:07:10 +0100

    Add initial implementation of function1.py and function1.xlsm for xlwings integration

---
+++ general_applic_related/office/excel/xlwings_related/quickstart/function1/function1.py
@@ -0,0 +1,21 @@
+import xlwings as xw
+from xlwings import func
+
+
+def main():
+    wb = xw.Book.caller()
+    sheet = wb.sheets[0]
+    if sheet["A1"].value == "Hello xlwings!":
+        sheet["A1"].value = "Bye xlwings!"
+    else:
+        sheet["A1"].value = "Hello xlwings!"
+
+
+@func
+def hello(name):
+    return f"Hello {name}!"
+
+
+if __name__ == "__main__":
+    xw.Book("function1.xlsm").set_mock_caller()
+    main()
