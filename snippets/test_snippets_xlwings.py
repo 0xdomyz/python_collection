b5c11d35d9081c6de581c3c84da412d7a096df45
0xdomyz <yzdominic@outlook.com>
2026-06-28 00:07:07 +0100

    Refactor xlwings snippets for improved readability and functionality

--- snippets/test_snippets_xlwings.py
+++ snippets/test_snippets_xlwings.py
@@ -12,11 +12,19 @@ import xlwings as xw
 xw.view(df)
 
 
+import pandas as pd
 # %%
-# xldf
 import xlwings as xw
 
-wb = xw.books.active # xw.Book()
-ws = wb.sheets.add('df2')
-ws["A1"].value = df
+ws2 = xw.books.active.sheets['Sheet1']
+df2 = ws2['A1'].expand().options(pd.DataFrame).value
+df2
+# %%
+import xlwings as xw
+
+ws = xw.books.active.sheets.add('df4')
+ws["A1"].value = df2
 ws.tables.add(source=ws["A1"].expand())
+
+# %%
+xw.books.active.close()
