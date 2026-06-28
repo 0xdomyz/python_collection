b0ad3389baf2271a989879b432ee4e1ad58e57db
0xdomyz <yzdominic@outlook.com>
2026-06-29 00:02:00 +0100

    Refactor pivot table configuration and formulas in binning_example.py for improved clarity and functionality

--- general_applic_related/office/excel/xlwings_related/pivot_dashboard/examples/binning_example.py
+++ general_applic_related/office/excel/xlwings_related/pivot_dashboard/examples/binning_example.py
@@ -41,62 +41,78 @@ logger.add(sys.stdout, level="DEBUG")
 # bins
 ps = dashboard.ws_pivot
 
-ps["B2"].value = 'age group'
-ps["C2"].value = 'min age'
-ps["D2"].value = 'max age'
+ps["B2"].value = 'bin'
+ps["C2"].value = 'bin_name'
+ps["D2"].value = 'gt'
+ps["E2"].value = 'le'
 
 start_value = 0
-for i in range(3, 3+10):
-    ps[f"B{i}"].formula = f"={ps[f'C{i}'].address}&\" - \"&{ps[f'D{i}'].address}"
-    ps[f"C{i}"].value = start_value
+start_row = 3
+for i in range(start_row, start_row+10):
+    ps[f"B{i}"].value = i-start_row+1
+    ps[f"C{i}"].formula = f"={ps[f'D{i}'].get_address(0,0)}&\" - \"&{ps[f'E{i}'].get_address(0,0)}"
+    if i == start_row:
+        ps[f"D{i}"].value = 0
+    else:
+        ps[f"D{i}"].formula = f"={ps[f'E{i-1}'].get_address(0,0)}"
     start_value += 10
-    ps[f"D{i}"].value = start_value
+    ps[f"E{i}"].value = start_value
 
 # %%
+cont_col = "age"
+disc_col = "age_group"
 tbl = dashboard.ws_data.tables[0]
-tbl.api.ListColumns.Add().Name = "age_group"
-col = "age"
+tbl.api.ListColumns.Add().Name = disc_col
 
 # Build nested IF formula in a loop
 rows = range(3, 13)
-formula = f"'{ps.name}'!{ps[f'B{rows[-1]}'].address}"
+formula = f"'{ps.name}'!{ps[f'C{rows[-1]}'].address}"
 
 for r in reversed(list(rows[:-1])):
     formula = (
-        f"IF([@{col}]<='{ps.name}'!{ps[f"D{r}"].address}, "
+        f"IF([{cont_col}]<='{ps.name}'!{ps[f"E{r}"].address}, "
         f"'{ps.name}'!{ps[f'B{r}'].address}, {formula})"
     )
 
-tbl.api.ListColumns("age_group").DataBodyRange.Formula = "=" + formula
+tbl.api.ListColumns(disc_col).DataBodyRange.Formula = "=" + formula
 
 # %%
+rate_col = "survived"
+time_col = 'fare_binned'
+test_cols = ['class','who']
 pivot_configs = [
     # fmt: off
-    dict(data_field=["n",'survived'],xl_func=['sum','average'],row_field="age_group",plot_on_2nd_axis='survived'),
-    dict(data_field=["n"], xl_func=['sum'],row_field="fare_binned",col_field="age_group",chart_type="area_stacked",),
-    dict(data_field=["survived"], xl_func=['average'],row_field="fare_binned",col_field="age_group",chart_type="line",),
-    dict(data_field=['survived'],xl_func=['average'],row_field="age_group",col_field='alone', chart_type="line"),
+    dict(data_field=["n", rate_col], xl_func=['sum','average'], row_field=disc_col, plot_on_2nd_axis=rate_col),
+    dict(data_field=["n"], xl_func=['sum'], row_field=time_col, col_field=disc_col, chart_type="area_stacked_100",),
+    dict(data_field=[rate_col], xl_func=['average'], row_field=time_col, col_field=disc_col, chart_type="line",),
     # fmt: on
 ]
+for col in test_cols:
+    pivot_configs += [
+        dict(data_field=[rate_col], xl_func=['average'], row_field=disc_col, col_field=col, chart_type="line"),
+    ]
 
 dashboard.add_pivots(
     pivot_configs,
     chart_layout={
-        "ncols": 1,
+        "ncols": 2,
         "left_offset": 500,
     },
     dest_layout={
-        "col": "AB",
+        "col": "AM",
         "row_step": 30,
-        "ncols": 1,
+        "col_step": 20,
+        "ncols": 2,
     },
 )
 # %%
 
 dashboard.add_slicers(
     fields=[
+        time_col,
+        "sibsp",
         "embark_town",
-        'who',
+        'sex',
     ],
     layout={
         "ncols": 2,
